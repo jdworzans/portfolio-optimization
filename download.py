d67a36a6d9ad3695083b11d0ad2fa4265c4c42e8
Jakub Dworzański <dworzanski.jakub@gmail.com>
2022-01-17 18:09:54 +0100

    Use WIG20 stocks from 2009 to avoid stocks with short history

--- download.py
+++ download.py
@@ -1,12 +1,18 @@
+from pathlib import Path
+
 import pandas as pd
 
+RAW_DATASETS_DIR = Path("data/raw")
 BASE_URL = "https://stooq.com/q/d/l/?s={ticker}"
-WIG20_TICKERS = ["ACP", "ALE", "CCC", "CDR", "CPS", "DNP", "JSW", "KGH", "LPP", "LTS", "MRC", "OPL", "PEO", "PGE", "PGN", "PKN", "PKO", "PZU", "SPL", "TPE"]
+# stocks from WIG20 (30.04.2009) which are still available
+TICKERS = ["PKO", "PEO", "OPL", "PKN", "KGH", "PGN", "ACP", "GTC", "SPL", "PBG", "CEZ", "GTN", "PXM", "MBK", "CPS", "LTS", "AGO", "BIO"]
+
+RAW_DATASETS_DIR.mkdir(exist_ok=True)
 
 def download_historical_data(ticker) -> pd.DataFrame:
     data_url = BASE_URL.format(ticker=ticker)
     return pd.read_csv(data_url)
 
 
-for ticker in WIG20_TICKERS:
-    download_historical_data(ticker).to_csv(f"data/raw/{ticker}.csv")
+for ticker in TICKERS:
+    download_historical_data(ticker).to_csv(RAW_DATASETS_DIR / f"{ticker}.csv")
