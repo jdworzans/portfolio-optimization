078f7256bfe78c827965c5d37db304dba8eb2eb3
Jakub Dworzański <dworzanski.jakub@gmail.com>
2022-01-21 11:20:39 +0100

    Add more tests for NSGA2 selection

--- test_selection.py
+++ test_selection.py
@@ -3,7 +3,7 @@ import numpy as np
 from selection import selection
 
 
-def test_selection():
+def test_selection_with_2_simple_fronts():
     example = np.array(
         [
             [1, 0],
@@ -13,3 +13,26 @@ def test_selection():
     )
     np.testing.assert_equal(selection(example, 2), np.array([0, 1]))
     np.testing.assert_equal(selection(example, 3), np.array([0, 1, 2]))
+
+def test_selection_with_3_simple_fronts():
+    example = np.array(
+        [
+            [1, 0],
+            [0, 1],
+            [1, 1],
+            [2, 2],
+        ]
+    )
+    np.testing.assert_equal(selection(example, 2), np.array([0, 1]))
+    np.testing.assert_equal(selection(example, 3), np.array([0, 1, 2]))
+
+def test_selection_with_nontrivial_front():
+    example = np.array(
+        [
+            [1, 0],
+            [0, 1],
+            [0.5, 0.5],
+        ]
+    )
+    assert set(selection(example, 2)) == {0, 1}
+    assert set(selection(example, 3)) == {0, 1, 2}
