549e211df5ec58f56219ebd602ac87c86fa11564
Jakub Dworzański <dworzanski.jakub@gmail.com>
2022-01-22 15:19:30 +0100

    Fix integration test

--- nsga2/integrations/test_nsga2.py
+++ nsga2/integrations/test_nsga2.py
@@ -1,7 +1,19 @@
+from functools import partial
+
+import numpy as np
 import pytest
-
 from nsga2.model import NSGA2
+from nsga2.objectives import (get_objective, portfolios_neg_returns,
+                              portfolios_vars)
 
-def test_model_work():    
+
+def test_model_work():
+    R = np.random.random(18)
+    SIGMA = np.random.random((18, 18))
+    SIGMA = SIGMA @ SIGMA.T
+    objective = get_objective(
+        partial(portfolios_neg_returns, R=R),
+        partial(portfolios_vars, SIGMA=SIGMA),
+    )
     nsga = NSGA2(18, 100, 10)
-    nsga.simulate(False)
+    nsga.simulate(objective, False)
