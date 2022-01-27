9cd66d22c247a996db730832a1a530b44f483c4a
Jakub Dworzański <dworzanski.jakub@gmail.com>
2022-01-27 11:32:08 +0100

    Analyse semivariance and VaR

--- nsga2/integrations/test_nsga2.py
+++ nsga2/integrations/test_nsga2.py
@@ -1,7 +1,6 @@
 from functools import partial
 
 import numpy as np
-import pytest
 from nsga2.model import NSGA2
 from nsga2.objectives import (get_objective, portfolios_neg_returns,
                               portfolios_semivariances, portfolios_vars)
@@ -11,10 +10,11 @@ def test_model_work():
     R = np.random.random(18)
     SIGMA = np.random.random((18, 18))
     SIGMA = SIGMA @ SIGMA.T
+    returns = np.random.randn(1000, 18)
     objective = get_objective(
         partial(portfolios_neg_returns, R=R),
         partial(portfolios_vars, SIGMA=SIGMA),
-        portfolios_semivariances,
+        partial(portfolios_semivariances, returns=returns),
     )
     nsga = NSGA2(18, 100, 10)
     nsga.simulate(objective, False)
--- nsga2/nsga2/objectives.py
+++ nsga2/nsga2/objectives.py
@@ -1,17 +1,15 @@
 import numpy as np
-import pandas as pd
 
-from nsga2.defaults import MERGED_DATASET_FILEPATH
-
-DEFAULT_RETURNS = np.asarray(pd.read_csv(MERGED_DATASET_FILEPATH, parse_dates=["Date"], index_col="Date").dropna())
 
 def portfolios_vars(weights: np.ndarray, SIGMA: np.ndarray) -> np.ndarray:
     return np.einsum("...i,ij,...j", weights, SIGMA, weights)
 
-def portfolios_neg_returns(weights: np.ndarray, R: np.ndarray) -> np.ndarray:
-    return - weights @ R
 
-def portfolios_semivariances(weights: np.ndarray, returns=DEFAULT_RETURNS):
+def portfolios_neg_returns(weights: np.ndarray, R: np.ndarray) -> np.ndarray:
+    return -weights @ R
+
+
+def portfolios_semivariances(weights: np.ndarray, returns: np.ndarray) -> np.ndarray:
     weights_returns = np.einsum("ij,kj", weights, returns)
     avg_returns = weights_returns.mean(axis=-1, keepdims=True)
     semivariances = np.mean(
@@ -21,7 +19,16 @@ def portfolios_semivariances(weights: np.ndarray, returns=DEFAULT_RETURNS):
     )
     return semivariances
 
+
+def portfolios_empirical_VaR(
+    weights: np.ndarray, returns: np.ndarray, alpha=0.95
+) -> np.ndarray:
+    weights_returns = np.einsum("ij,kj", weights, returns)
+    return -np.quantile(weights_returns, 1 - alpha, axis=-1)
+
+
 def get_objective(*objectives):
     def objective(weights: np.ndarray) -> np.ndarray:
         return np.stack([obj(weights) for obj in objectives], axis=-1)
+
     return objective
